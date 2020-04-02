0561860f86e287a385032e8dc989328d578c51d2
Michaela Bateman <michaela.bateman001@gmail.com>
2020-04-02 18:01:27 -0500

    Initial commit

---
+++ brent.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+	$(".main").hide();
+	
+	//show super cool splash page
+	
+	setTimeout(function(){
+		$(".main").css("background-color", "#DFB0CD");
+		$(".main").show();
+		$(".overlay").hide();
+	}, 5.47*1000);
